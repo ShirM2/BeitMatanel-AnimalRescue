a9524e303bc679079c325aaf12617c7823837ea6
ShirM2 <shirmalkas123@gmail.com>
2026-05-03 11:07:45 +0300

    update

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,11 +25,11 @@ const Home = () => {
         <div className="absolute inset-0 flex flex-col justify-center items-center pb-43">
           <h1 className="text-6xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">מצילים,מטפלים ומאמצים חיות מחמד בישראל</h1>
           <p className="mt-3 text-2xl text-white drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)]">עמותת בית מתנאל מחלצת ומשקמת חיות מחמד במצוקה ומוצאת להם בתים אוהבים</p>
-          <a href="/adpot_form" 
-              className="mt-3 bg-white text-yellow-500 px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-yellow-100 transition-all duration-300 whitespace-nowrap text-xs md:text-base">אני רוצה לאמץ</a>
+          <Link to="/adpot_form" 
+              className="mt-3 bg-white text-yellow-500 px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-yellow-100 transition-all duration-300 whitespace-nowrap text-xs md:text-base">אני רוצה לאמץ</Link>
 
-          <a href="/report" 
-              className="mt-3 bg-red-700 text-yellow-500 px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-red-800 transition-all duration-300 whitespace-nowrap text-xs md:text-base">דווח על חיה במצוקה⚠️</a>
+          <Link to="/ReportForm" 
+              className="mt-3 bg-red-700 text-yellow-500 px-3 py-1.5 md:px-6 md:py-2 rounded-full font-bold shadow-md hover:bg-red-800 transition-all duration-300 whitespace-nowrap text-xs md:text-base">דווח על חיה במצוקה⚠️</Link>
         </div>
       </header>
 
